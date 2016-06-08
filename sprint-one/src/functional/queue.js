9c4b438c04100be74b1a0bcfc5b17056a6826fe0
jo-im <joann_im@hotmail.com>
2016-06-08 15:59:54 -0700

    Completed queue functional

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -3,16 +3,32 @@ var Queue = function() {
 
   // Use an object with numeric keys to store values
   var storage = {};
-
+  var length = 0;
+  var head, tail;
   // Implement the methods below
 
   someInstance.enqueue = function(value) {
+    if (length === 0) {
+      head = 0;
+      tail = 0;
+    } else {
+      tail++;
+    }
+    length++;
+    storage[tail] = value;
   };
 
   someInstance.dequeue = function() {
+    
+    var temp = storage[head];
+    head++;
+
+    length = Math.max(0, length-1);
+    return temp;
   };
 
   someInstance.size = function() {
+    return length;
   };
 
   return someInstance;
