e7d76dc857b1c4b6921f550e03b881188ac9869b
jo-im <joann_im@hotmail.com>
2016-06-08 16:19:24 -0700

    Complete functional-shared stack

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,34 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var someInstance = {
+    key: 0,
+    storage: {}
+  };
+  
+  // Use an object with numeric keys to store values
+  _.extend(someInstance, stackMethods);
+
+  return someInstance;
 };
 
-var stackMethods = {};
+var stackMethods = {
+
+  push : function(value) {
+    this.key++;
+    this.storage[this.key] = value;
+  },
+
+  pop : function() {
+    var temp = this.storage[this.key];
+    delete this.storage[this.key];
+    this.key = Math.max(0, this.key - 1);
+    return temp;
+  },
+
+  size : function() {
+    return this.key;
+  }
+};
 
 
