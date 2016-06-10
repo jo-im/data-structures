53bcd8606c03c8d1dcc3bfc2184239c1c3d7aa3b
jo-im <joann_im@hotmail.com>
2016-06-10 12:26:43 -0700

    Complete doublyLinkedList

--- sprint-two/src/doublyLinkedList.js
+++ sprint-two/src/doublyLinkedList.js
@@ -10,13 +10,18 @@ var DoublyLinkedList = function() {
       list.head = list.tail;
     } else {
       (list.tail).next = Node(value);
+      var tempNode = list.tail;
       list.tail = (list.tail).next;
+      list.tail.previous = tempNode;
     }
   };
 
   list.removeHead = function() {
     var temp = list.head;
     list.head = list.head.next;
+    if (list.head) {
+      list.head.previous = null;
+    }
     return temp.value;
   };
 
@@ -35,12 +40,32 @@ var DoublyLinkedList = function() {
   };
 
   list.addToHead = function(value) {
-
+    //first need to check for null head (this means no nodes in list)
+    if (list.head === null) {
+      list.head = Node(value);
+      list.tail = list.head;
+    } else {
+      //have list.head equal to a new object created by Node
+      //list.head.next is equal to the previous head
+      list.head.previous = Node(value);
+      var tempNode = list.head;
+      list.head = (list.head).previous;
+      list.head.next = tempNode;
+    }
+    
   };
 
-  list.removeTail = function(value) {
-
+  list.removeTail = function() {
+    //have list.tail equal to the previous value of list.tail
+    //set list.tail.next to null
+    var temp = list.tail;
+    list.tail = list.tail.previous;
+    if (list.tail) {
+      list.tail.next = null;
+    }
+    return temp.value; 
   };
+
   return list;
 };
 
@@ -49,6 +74,7 @@ var Node = function(value) {
 
   node.value = value;
   node.next = null;
+  node.previous = null;
 
   return node;
 };
