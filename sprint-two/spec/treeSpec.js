07c15df036f088feaffd02c8b47d90196bf13e6a
jo-im <joann_im@hotmail.com>
2016-06-10 21:25:14 -0700

    Completed advanced tree prompts

--- sprint-two/spec/treeSpec.js
+++ sprint-two/spec/treeSpec.js
@@ -1,8 +1,14 @@
 describe('tree', function() {
   var tree;
 
+  //example callback for testing purposes
+  //exampleCB converts all node.value to strings
+  var exampleCB = function(node) {
+    node.value = node.value + '';
+  };
+
   beforeEach(function() {
-    tree = Tree();
+    tree = Tree();   
   });
 
   it('should have methods named "addChild" and "contains", and a property named "value"', function() {
@@ -39,6 +45,45 @@ describe('tree', function() {
     tree.children[1].addChild(8);
     expect(tree.contains(7)).to.equal(true);
     expect(tree.contains(8)).to.equal(true);
+  });
+
+
+  //New tests for advanced portions
+  it('should add parent to each node unless the node is the only one', function() {
+    tree.addChild(5);
+    expect(tree.children[0].parent).to.equal(null);
+    tree.children[0].addChild(6);
+    tree.children[0].addChild(4);
+    expect(tree.children[0].children[0].parent.value).to.equal(5);
+    expect(tree.children[0].children[1].parent.value).to.equal(5);
+  });
+
+  it('should traverse the tree and execute a callback on each node', function() {
+    tree.addChild(5);
+    tree.addChild(6);
+    tree.children[0].addChild(7);
+    tree.children[1].addChild(8);
+    tree.traverse(exampleCB, tree);
+    expect(tree.contains('5')).to.equal(true);
+    expect(tree.contains('6')).to.equal(true);
+    expect(tree.contains('7')).to.equal(true);
+    expect(tree.contains('8')).to.equal(true);
+  });
+
+  it('should split node from its parent into its own tree when removeFromParent is invoked', function() {
+    tree.addChild(5);
+    tree.children[0].addChild(2);
+    tree.children[0].addChild(8);
+    tree.children[0].children[0].addChild(1);
+    tree.children[0].children[0].addChild(3);
+    tree.children[0].children[1].addChild(7);
+    tree.children[0].children[1].addChild(9);
+    //Dissasociated tree should still contain its child nodes
+    expect(tree.removeFromParent(2).contains(1)).to.equal(true);
+    //Old tree should still contain the branches that weren't pruned
+    expect(tree.contains(8)).to.equal(true);
+    //Old tree shouldn't contain pruned children
+    expect(tree.contains(3)).to.equal(false);
   });
 
 });
--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -4,7 +4,8 @@ var Tree = function(value) {
   newTree.value = value;
 
   // your code here
-  newTree.children = [];  // fix me
+  newTree.children = [];
+  newTree.parent = null;
 
   return newTree;
 };
@@ -12,7 +13,25 @@ var Tree = function(value) {
 var treeMethods = {};
 
 treeMethods.addChild = function(value) {
-  this.children.push(Tree(value));
+  var child = Tree(value);
+  if (this.value) {
+    child.parent = this;
+  }
+  this.children.push(child);
+};
+
+treeMethods.traverse = function(callback, node) {
+  node = node || this;
+  
+  if (node.value) {
+    //Are we able to return the callback here, or just execute without a return statement?
+    callback(node);
+  }
+
+  for (var i = 0; i < node.children.length; i++) {
+    this.traverse(callback, node.children[i]);
+  }
+
 };
 
 treeMethods.contains = function(target, node, result) {
@@ -22,13 +41,34 @@ treeMethods.contains = function(target, node, result) {
     result = true;
   }
 
-  for (var i=0; i<node.children.length; i++) {
+  for (var i = 0; i < node.children.length; i++) {
     result = this.contains(target, node.children[i], result);
   }
 
   return result;
 };
 
+treeMethods.removeFromParent = function(target) {
+  //disassociate node from parent and create two discrete trees
+  var newParent;
+
+  var removalCallback = function(nodeObject) {
+    if (nodeObject.value === target) {
+      //Split the child off into its own family :_) goodbye baby bird
+      newParent = nodeObject;
+      //Remove the child from the parent 
+      var oldParent = newParent.parent;
+      var removedChildIndex = oldParent.children.indexOf(newParent);
+      oldParent.children.splice(removedChildIndex, 1);
+      //Set the new parent's parent to null
+      newParent.parent = null;
+    }
+  };
+
+  this.traverse(removalCallback, this);
+
+  return newParent;
+};
 
 
 /*
